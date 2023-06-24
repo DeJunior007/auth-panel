7d3d1e34d8ce8fd66f07edec15c8603d0aab6ac7
DeJunior007 <dejunior05@hotmail.com>
2023-06-23 22:16:54 -0300

    Feat: instalação de dependências

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Auth_login</title>
+      </Head>
+      <main>
+        <section className="w-full h-auto py-2 flex justify-center text-center font-bold text-3xl text-slate-100">
+          Hello world
+        </section>
+        <form>
+          <input />
+          <input />
+          <input />
+          <input />
+        </form>
+      </main>
+    </>
+  );
+}
